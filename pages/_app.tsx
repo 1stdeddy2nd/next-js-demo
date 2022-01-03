dbaf4df3dc30179d6aa8aca742dd0b09e1f997ef
1stdeddy2nd <deddysetiawan17@gmail.com>
2022-01-03 16:06:39 +0700

    [fixing]

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,9 @@
 import "../styles/globals.css";
-import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 // eslint-disable-next-line react/jsx-props-no-spreading
 export default function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    typeof document !== undefined
-      ? require("bootstrap/dist/js/bootstrap")
-      : null;
-  }, []);
   return <Component {...pageProps} />;
 }
